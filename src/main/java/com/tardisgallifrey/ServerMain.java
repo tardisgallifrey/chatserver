25ea568e75acd695df9297dad2af5c81c2c64203
tardisgallifrey <bigmanrunning@gmail.com>
2024-05-06 16:47:25 -0500

    fixed commit

--- src/main/java/com/tardisgallifrey/ServerMain.java
+++ src/main/java/com/tardisgallifrey/ServerMain.java
@@ -9,9 +9,13 @@ public class ServerMain {
 
     public static void main(String[] args) {
         System.out.println("Type ctrl-C to quit\n");
+<<<<<<< HEAD
         Runnable server1 = new ChatServer("ChatServer",6100);
         ExecutorService servers = Executors.newFixedThreadPool(15);
         servers.submit(server1);
+=======
+        Runnable server1 = new ChatServer("Server One", 6100);
+>>>>>>> 2e07996 (unsupported commit)
 
 
 
